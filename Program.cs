e95907d523d28110057ed6470ff96f683052f385
20100 <mmmelkady23@gmail.com>
2025-01-16 15:32:48 +0200

    inheritance and relations

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace OOP_4
+﻿using OOP_4.Inhertance;
+using System.Linq.Expressions;
+
+namespace OOP_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
+            #region inhertance
+            //Inheritance [IS A]
+            // single inhertance not mulitple( like Diamond problem)
+
+
+            /* 
+             Parent p1 = new Parent(10, 30);
+             Console.WriteLine(p1);
+             Console.WriteLine(p1.productNumber());            
+             */
+            /*
+            Child child = new Child(12, 324, 5);     
+            Console.WriteLine("---------");
+            Console.WriteLine(child);
+            Console.WriteLine("----------");
+            Console.WriteLine(child.productNumber());
+           */
+            #endregion
+            #region Access
+            //Access modifiers
+            //1- private
+            //2- private protected
+            //3- protected
+            //4- internal 
+            //5- protected internal
+            //6- public 
+
+            ////protected best in inheritance project
+            //car car = new car(2, 3, 4, 5, 6, 7);
+            //// A is public and other no
+            //Console.WriteLine(car);
+
+            #endregion
+
+            #region MyRegion
+
+            #endregion
+
         }
     }
 }
---
+++ car.cs
@@ -0,0 +1,21 @@
+﻿using Acess_mod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_4
+{
+    internal class car : typeA
+    {
+        public car(int _A, int _B, int _c, int _d, int _E, int _F) : base(_A, _B, _c, _d, _E, _F)
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"A {A},B {"no"},  C  {C} ,D {"D is internal"} ,E {"private protected "} ,F {F}";
+        }
+    }
+}
